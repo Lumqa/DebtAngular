372457a891f8b0c2d9fb31d3242b42b7f727e33d
Artem <woullbors96@yandex.ru>
2019-02-18 17:59:59 +0300

    Basic implementation "Change Sum" - not work if use Add New Member(Not load DOM)

--- DebtAngular/ClientApp/src/app/tasks/AddOrEditTask/addOrEditTasks.component.ts
+++ DebtAngular/ClientApp/src/app/tasks/AddOrEditTask/addOrEditTasks.component.ts
@@ -8,11 +8,15 @@ import { Task } from './../../_models';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscriber } from 'rxjs';
 import { elementStyleProp } from '@angular/core/src/render3';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+declare var task: any;
+
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './addOrEditTasks.component.html'
 })
+
 export class AddOrEditTasksComponent implements OnInit {
   taskId: string;
   task: Task;
@@ -40,13 +44,19 @@ export class AddOrEditTasksComponent implements OnInit {
     }
   }
 
-  fillingMemberData(member:Member){
+  addMember(){
+    this.addTaskMember();
+    debugger;
+    this.changeSum();
+  }
+
+  fillingMemberData(member: Member) {
     let fg = this.formBuilder.group({
-      id:[member.id],
-      name:[member.name, Validators.required],
-      deposit:[member.deposit, [Validators.required,Validators.min(0)]],
-      debt:[member.debt,[Validators.required,Validators.min(0)]],
-      taskId:[member.taskId]
+      id: [member.id],
+      name: [member.name, Validators.required],
+      deposit: [member.deposit, [Validators.required, Validators.min(0)]],
+      debt: [member.debt, [Validators.required, Validators.min(0)]],
+      taskId: [member.taskId]
     });
     this.taskMembers.push(fg);
   }
@@ -56,9 +66,9 @@ export class AddOrEditTasksComponent implements OnInit {
       taskId: [''],
       userId: [''],
       name: ['', Validators.required],
-      sum: ['0', [Validators.required, Validators.min(0), Validators.pattern('^[0-9]{0,10}$')]],
+      sum: ['0', [Validators.required, Validators.min(0)]],
       members: this.formBuilder.array([])
-    }, { validator: this.SumValidator, validateName: this.MemberNameValidator });
+    }, { validator: this.SumValidator});
     //this.addTaskMember();
   }
 
@@ -68,8 +78,8 @@ export class AddOrEditTasksComponent implements OnInit {
     let sumDeposit = 0;
     let sumDebt = 0;
     fg.get('members').value.forEach(element => {
-      sumDeposit += parseFloat((<Member>element).deposit.toString());
-      sumDebt += parseFloat((<Member>element).debt.toString());
+      sumDeposit += (<Member>element).deposit;
+      sumDebt += (<Member>element).debt;
     });
 
 
@@ -90,10 +100,6 @@ export class AddOrEditTasksComponent implements OnInit {
       }
     }
   }
-
-  MemberNameValidator(fg: FormGroup) {
-  }
-
 
   deleteMember(index: number) {
     this.taskMembers.removeAt(index);
@@ -124,5 +130,107 @@ export class AddOrEditTasksComponent implements OnInit {
     });
     this.taskService.save(this.task).subscribe();
     this.router.navigate(['tasks']);
+  }
+
+  // changeSumAngular(){
+  //   var sumInputElem = document.getElementById("Sum");
+  //   var sumInput = sumInputElem['value'];
+  // }
+
+  //JS OPERATIONS
+  changeSum() {
+    //value in sum field
+    var sumInputElem = document.getElementById("Sum");
+    var sumInput = sumInputElem['value'];
+
+    //change first field "deposit"
+    var depositInputs = document.getElementsByClassName("deposit");
+    console.log(depositInputs.length);
+    depositInputs[0]['value'] = sumInput;
+    this.changeDeposit();
+
+    //get all inputs with manual edit value
+    var debtsEditInputs = document.getElementsByClassName("debtediting");
+
+    if (debtsEditInputs.length != 0) {
+      var debtsEditSum = 0;
+      //caclulate sum without this fields
+      debtsEditSum = this.CalculateSum(debtsEditInputs, debtsEditInputs.length);
+
+      sumInput -= debtsEditSum;
+      if (sumInput < 0) {
+        sumInputElem.style.color = "red";
+        return;
+      } else {
+        sumInputElem.style.color = "black";
+      }
+    }
+
+    //get all inputs with "debts"
+    var debtsInputs = document.getElementsByClassName("debt");
+    var debtsLength = debtsInputs.length;
+
+    //calculate basic value
+    var basicValueWithError = parseFloat((sumInput / debtsLength).toFixed(2));
+
+    //calculate sum value with error
+    var sumWithError = basicValueWithError * debtsLength;
+
+    //get error
+    var error = parseFloat((sumInput - sumWithError).toFixed(2));
+
+    //global iterator for set correct debt value
+    var iterator = 0;
+    if (error != 0) {
+      //check sing in error
+      var sign = (error >= 0) ? true : false;
+      while (error != 0) {
+        //check sign and inc or dec value for correct value
+        switch (sign) {
+          case false:
+            error += 0.01;
+            debtsInputs[iterator]['value'] = (basicValueWithError - 0.01).toFixed(2);
+            break;
+
+          case true:
+            error -= 0.01;
+            debtsInputs[iterator]['value'] = (basicValueWithError + 0.01).toFixed(2);
+            break;
+        }
+        iterator++;
+      }
+    }
+
+    //set value for other inputs, if needed
+    while (iterator < debtsLength) {
+      debtsInputs[iterator]['value'] = basicValueWithError;
+      iterator++;
+    }
+  }
+
+  changeDebt(editedInput) {
+    editedInput.className += "editing";
+  }
+
+
+  changeDeposit() {
+    var sumInput = document.getElementById("Sum");
+    var depositInputs = document.getElementsByClassName("deposit");
+
+    var sumDeposits = this.CalculateSum(depositInputs, depositInputs.length);
+    var sumInput = document.getElementById("Sum");
+    if (sumDeposits !== sumInput['value']) {
+      sumInput.style.color = "red";
+    } else {
+      sumInput.style.color = "black";
+    }
+  }
+  //replace all sum calc
+  CalculateSum(inputs, length) {
+    var sum = 0;
+    for (var iterator = 0; iterator < length; iterator++) {
+      sum += parseFloat(inputs[iterator].value);
+    }
+    return sum;
   }
 }
